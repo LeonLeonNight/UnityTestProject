799090202ca5ed0c8daadec632e755c8280ff4e2
soloviev-nikolay <soloviev_nikolay@mail.ru>
2021-04-06 01:22:32 +0500

    Initial commit

---
+++ New Unity Project/Assets/Scripts/Item.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    /// <summary>
+    /// Id уникального предмета
+    /// </summary>
+    public int Id;
+    /// <summary>
+    /// Имя предмета
+    /// </summary>
+    public string NameItem;
+
+    /// <summary>
+    /// Количество
+    /// </summary>
+    public int CountItem;
+
+    /// <summary>
+    /// СтАкающие предмет или нет
+    /// </summary>
+    public bool isStackable;
+
+    [Multiline(5)]
+    public string Description;
+
+    //Изображение предмета и его префаб получим другим способом
+    //Будем указывать его из папки Resources, т.е. абсолютным путем
+
+    public string pathIcon;
+    public string pathPrefab;
+}
